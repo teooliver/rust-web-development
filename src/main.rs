80bdf2ea8d6697dfe54a8996c1241868e8203fcb
Teo Oliver <teo.liz.oli@gmail.com>
2022-01-12 00:00:07 +0000

    notes: p.54 -> handling CORS headers

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use serde::Serialize;
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
-use warp::Filter;
+use warp::{http::StatusCode, reject::Reject, Filter, Rejection, Reply};
+
+#[derive(Debug)]
+struct InvalidId;
+impl Reject for InvalidId {}
 
 #[derive(Debug, Serialize)]
 struct Question {
@@ -43,8 +47,30 @@ async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
         Some(vec!["faq".to_string()]),
     );
 
+    match question.id.0.is_empty() {
+        true => Err(warp::reject::custom(InvalidId)),
+
+        false => Ok(warp::reply::json(&question)),
+    };
+
     Ok(warp::reply::json(&question))
 }
+
+async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
+    if let Some(InvalidId) = r.find() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented",
+            StatusCode::UNPROCESSABLE_ENTITY,
+        ))
+    } else {
+        Ok(warp::reply::with_status(
+            "Route not found",
+            StatusCode::NOT_FOUND,
+        ))
+    }
+}
+
+
 
 #[tokio::main]
 async fn main() {
@@ -60,7 +86,8 @@ async fn main() {
     let get_items = warp::get()
         .and(warp::path("questions"))
         .and(warp::path::end())
-        .and_then(get_questions);
+        .and_then(get_questions)
+        .recover(return_error);
 
     let routes = get_items;
 
